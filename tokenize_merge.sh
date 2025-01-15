e7d3fdac83fcd0e0fbaa008442f3abaf339130e8
SHUMKASHUN <ksshumab@connect.ust.hk>
2025-01-16 00:59:02 +0800

    hotfix: remember to revert back

--- tokenize_merge.sh
+++ tokenize_merge.sh
@@ -16,34 +16,34 @@ fi;
 
 
 # Find all files in the directory and process them in parallel
-find "$merge_dir" -maxdepth 1 -type f -name "*.json*" | xargs -P "$num_processes" -I {} bash -c '
-    preprocess_file() {
-        file="$1"
+# find "$merge_dir" -maxdepth 1 -type f -name "*.json*" | xargs -P "$num_processes" -I {} bash -c '
+#     preprocess_file() {
+#         file="$1"
 
-        prefixfile=${file%%.json*}
-        prefixfile=${prefixfile##*/}
-        recovered_dir=${file%/*}
-        echo "Processing $prefixfile"
-        newname=${recovered_dir##*/}
-        python tools/preprocess_data.py \
-                   --input $recovered_dir/$prefixfile.jsonl \
-                   --output-prefix ${prefixfile}_${newname} \
-                   --tokenizer-model neo/tokenizer.model \
-                   --tokenizer-type SentencePieceTokenizer \
-                   --keep-sequential-samples \
-                   --append-eod \
-                   --workers 32 \
-                   --json-keys "text"
-        echo $newname
-        mv ./${prefixfile}_${newname}_text_document.bin /mnt/hdfs/byte_tiktok_aiic/user/huangyuzhen/data_selection/data/${newname}/
-        mv ./${prefixfile}_${newname}_text_document.idx /mnt/hdfs/byte_tiktok_aiic/user/huangyuzhen/data_selection/data/${newname}/
+#         prefixfile=${file%%.json*}
+#         prefixfile=${prefixfile##*/}
+#         recovered_dir=${file%/*}
+#         echo "Processing $prefixfile"
+#         newname=${recovered_dir##*/}
+#         python tools/preprocess_data.py \
+#                    --input $recovered_dir/$prefixfile.jsonl \
+#                    --output-prefix ${prefixfile}_${newname} \
+#                    --tokenizer-model neo/tokenizer.model \
+#                    --tokenizer-type SentencePieceTokenizer \
+#                    --keep-sequential-samples \
+#                    --append-eod \
+#                    --workers 32 \
+#                    --json-keys "text"
+#         echo $newname
+#         mv ./${prefixfile}_${newname}_text_document.bin /mnt/hdfs/byte_tiktok_aiic/user/huangyuzhen/data_selection/data/${newname}/
+#         mv ./${prefixfile}_${newname}_text_document.idx /mnt/hdfs/byte_tiktok_aiic/user/huangyuzhen/data_selection/data/${newname}/
 
-        echo "Finished processing $prefixfile"
+#         echo "Finished processing $prefixfile"
 
-    }   
+#     }   
 
-    preprocess_file "$0"
-' "{}"
+#     preprocess_file "$0"
+# ' "{}"
 
 
 echo "All files processed"
@@ -51,6 +51,14 @@ echo "All files processed"
 INPUTPATH=${name}
 OUTPUT_PREFIX=$4
 OUTPUTPATH="${name}-merge"
+if [ ! -d "/mnt/hdfs/byte_tiktok_aiic/user/huangyuzhen/data_selection/data/${OUTPUTPATH}" ]
+then
+    echo "not exist"
+    mkdir -p /mnt/hdfs/byte_tiktok_aiic/user/huangyuzhen/data_selection/data/${OUTPUTPATH}
+else
+    echo "exist"
+fi;
+
 if [ ! -d "./data/${OUTPUTPATH}" ]
 then
     echo "not exist"
